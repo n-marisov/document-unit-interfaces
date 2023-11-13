8408e32df06dd8ddaebd15eca8def0ccb93b9958
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-11-13 14:04:42 +0300

    В утилиту ИНН добавлены методы для получения информации об ИНН.

--- src/Utility/InnNumberUtilityInterface.php
+++ src/Utility/InnNumberUtilityInterface.php
@@ -2,6 +2,7 @@
 
 namespace Maris\Interfaces\Document\Utility;
 
+use Maris\Interfaces\Document\AggregateNotNull\InnNumberAggregateNotNullInterface;
 use Maris\Interfaces\Document\Formatter\InnNumberFormatterInterface;
 use Maris\Interfaces\Document\Model\InnNumberInterface;
 use Maris\Interfaces\Document\UnitUtilityInterface;
@@ -17,4 +18,46 @@ interface InnNumberUtilityInterface extends UnitUtilityInterface, InnNumberForma
      * @return InnNumberInterface|null
      */
     public function create(Stringable|string $number): ?InnNumberInterface;
+
+    /***
+     * Указывает что ИНН принадлежит физическому лицу или ИП.
+     * @param InnNumberInterface|InnNumberAggregateNotNullInterface $inn
+     * @return bool
+     */
+    public function isPerson( InnNumberInterface|InnNumberAggregateNotNullInterface $inn ):bool;
+
+    /***
+     * Указывает что ИНН принадлежит организации.
+     * @param InnNumberInterface|InnNumberAggregateNotNullInterface $inn
+     * @return bool
+     */
+    public function isLegal( InnNumberInterface|InnNumberAggregateNotNullInterface $inn ):bool;
+
+    /***
+     * Указывает что ИНН принадлежит субъекту Российской организации.
+     * Иными словами принадлежит не зарубежной организации.
+     * Возвращает правильный результат для Юр лиц получивших
+     * ИНН не раньше 01.01.2005г.
+     * @param InnNumberInterface|InnNumberAggregateNotNullInterface $inn
+     * @return bool
+     */
+    public function isRussian(  InnNumberInterface|InnNumberAggregateNotNullInterface $inn ):bool;
+
+    /**
+     * Возвращает код подразделение ФНС выдавшего ИНН.
+     * Первые четыре цифры.
+     * Для иностранного юр лица "9909".
+     * @param InnNumberInterface|InnNumberAggregateNotNullInterface $inn
+     * @return string
+     */
+    public function fnsCode( InnNumberInterface|InnNumberAggregateNotNullInterface $inn ):string;
+
+    /***
+     * Возвращает порядковый номер постановки на учет.
+     * Для ФИ и ИП с 5 по 10 включительно цифры.
+     * Для Юр лица с 5 по 9 включительно цифры.
+     * @param InnNumberInterface|InnNumberAggregateNotNullInterface $inn
+     * @return string
+     */
+    public function position( InnNumberInterface|InnNumberAggregateNotNullInterface $inn ):string;
 }
